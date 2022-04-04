6d9056d42cf1c08bb95d85a70e5e9bd761c20103
Julie <julie.ribeiro.silva@gmail.com>
2022-04-04 14:44:37 -0300

    change home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:chat/pages/auth_page.dart';
-import 'package:chat/pages/loading_page.dart';
+import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AuthPage(),
+      home: AuthOrAppPage(),
     );
   }
 }
