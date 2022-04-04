c368fdc3f1216c0321b48de2616333ba5dca71ad
Julie <julie.ribeiro.silva@gmail.com>
2022-04-04 15:04:14 -0300

    construtor factory

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,6 @@
 import 'package:chat/components/auth_form.dart';
 import 'package:chat/core/models/auth_form_data.dart';
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -15,9 +15,9 @@ class _AuthPageState extends State<AuthPage> {
     try {
       setState(() => _isLoading = true);
       if (formData.isLogin) {
-        AuthMockService().login(formData.email, formData.password);
+        await AuthService().login(formData.email, formData.password);
       } else {
-        AuthMockService().signup(
+        await AuthService().signup(
             formData.name, formData.email, formData.password, formData.image);
       }
     } catch (error) {
