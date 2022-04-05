d57f801931d60fc724a0dfd6337ed889c4aaa524
Julie <julie.ribeiro.silva@gmail.com>
2022-04-05 14:22:14 -0300

    393 - Page Notification

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -3,7 +3,10 @@
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   @override
@@ -12,35 +15,62 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cod3r Chat'),
         actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Colors.white,
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                      child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('Sair')
+                    ],
+                  )),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
+              },
             ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Container(
-                    child: Row(
-                  children: [
-                    Icon(
-                      Icons.exit_to_app,
-                      color: Colors.black87,
+          ),
+          Stack(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (ctx) {
+                      return NotificationPage();
+                    }));
+                  },
+                  icon: Icon(Icons.notifications)),
+              Positioned(
+                  top: 5,
+                  right: 5,
+                  child: CircleAvatar(
+                    maxRadius: 10,
+                    backgroundColor: Colors.red,
+                    child: Text(
+                      Provider.of<ChatNotificationService>(context)
+                          .itemsCount
+                          .toString(),
+                      style: TextStyle(fontSize: 12),
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text('Sair')
-                  ],
-                )),
-              )
+                  )),
             ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().logout();
-              }
-            },
-          )
+          ),
         ],
       ),
       body: SafeArea(
---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Minhas notificações')),
+      body: Center(child: Text('Notificações')),
+    );
+  }
+}
